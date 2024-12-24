617f5ab5f43bdac34c949ed308ac1106227f7872
crystal <su000823@naver.com>
2024-12-24 14:31:09 +0900

    feat: infoApi 생성

---
+++ Safeevenonholidays-react/src/api/infoApi.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export const API_SERVER_HOST = 'http://localhost:8080'
+
+const prefix = `${API_SERVER_HOST}/api/info`
+
+//특정번호 조회
+export const getOne = async(id) => {
+    const res = await axios.get(`${prefix}/${id}`)
+    return res.data
+}
+
+//전체 list 조회
+export const getList = async(pageParam) => {
+    const {page, size} = pageParam
+    const res = await axios.get(`${prefix}/list`, {params : {page : page, size : size}})
+    return res.data
+}
+
+//데이터 추가
+export const postAdd = async(infoobj) => {
+    const res = await axios.post(`${prefix}/`, infoobj)
+    return res.data
+}
+
+//수정
+export const putOne = async(info) => {
+    const res = await axios.put(`${prefix}/${info.id}`, info)
+    return res.data
+}
+
+//삭제
+export const deleteOne = async(info) => {
+    const res = await axios.delete(`${prefix}/${info}`)
+    return res.data
+}
