8a42c5353b28f6cf03c56f8e88f057d446bf9c5b
dahyun <kkdh0130@gmail.com>
2024-12-24 21:51:49 +0900

    폰트, 리뷰, 즐겨찾기 추가

--- Safeevenonholidays-react/src/MyPage/MyPage.js
+++ Safeevenonholidays-react/src/MyPage/MyPage.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import "../styles/main.css";
+import { Button, Row } from "react-bootstrap";
 
 const MyPage = () => {
   return (
     <>
-    <div>마이페이지</div>
+      <div className="container mt-4 mb-4">
+        <p className='mypage-title'>마이 페이지</p>
+        <Row>
+          <div className="col-md-6">
+            <div className="card section1-card-left">
+              <h5 className="card-title p-4">내 프로필</h5>
+              <div className="card-body mypage-card-body">
+                <img
+                  src='/images/user.png'
+                  alt='userprofile'
+                  className="mypage-card-img"
+                />
+                <h5 className="mypage-card-title">이기자</h5>
+                <p className="mypage-card-text">test@email.com</p>
+                <Button variant="primary" className="position-absolute bottom-0 end-0 mb-3 me-3 w-25">수정</Button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 d-flex flex-column">
+            <div className="card mb-2 section1-card-right">
+              <div className="card-body">
+                <h5 className="card-title">MY 즐겨찾기</h5>
+                <p className="card-text">내가 즐겨찾기한 병원 목록을 확인해보세요.</p>
+              </div>
+            </div>
+            <div className="card section1-card-right">
+              <div className="card-body">
+                <h5 className="card-title">MY 리뷰 관리</h5>
+                <p className="card-text">내가 남긴 리뷰 목록을 확인해보세요.</p>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </div>
     </>
   )
 }
 
-export default MyPage
+export default MyPage;
