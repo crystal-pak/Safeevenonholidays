5cbb13cbf261ce6014809ab26dd1757b1c525d16
dahyun <kkdh0130@gmail.com>
2024-12-27 15:36:02 +0900

    스타일, 검색, 어드민 페이지 추가

--- Safeevenonholidays-react/src/MyPage/MyPage.js
+++ Safeevenonholidays-react/src/MyPage/MyPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/main.css";
+import "../styles/mypage.css";
 import { Button, Row } from "react-bootstrap";
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
