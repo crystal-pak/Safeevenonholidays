980b958dedd1bff65c2a35ac4569ca013c9605e8
seungwoo <tmddn0095@naver.com>
2025-01-06 17:01:36 +0900

    로그인,회원가입 디자인

--- Safeevenonholidays-react/src/UserAccount/Signup.js
+++ Safeevenonholidays-react/src/UserAccount/Signup.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import SignupComponent from '../component/member/SignupComponent'
+import React from 'react';
+import SignupComponent from '../component/member/SignupComponent';
 
 const Signup = () => {
   return (
     <>
       <SignupComponent />
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
